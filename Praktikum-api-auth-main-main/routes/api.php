8a96ffbf5588ab7f9606c847ab78df7f06a4e259
Your Name <you@example.com>
2023-07-10 11:24:14 +0700

    first commit

---
+++ Praktikum-api-auth-main-main/routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\API\AuthController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::get('/listproducts', [ProductsController::class,'index']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/listproducts', [ProductController::class, 'index']);
+});
+// Route::get('/login', [Controller::class,'login']);
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group( function () {
+	Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::resource('products', ProductsController::class);
+});
